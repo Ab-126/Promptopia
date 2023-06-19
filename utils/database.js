fb37c0b6c924b6555e697457a1a233ad27c7d9e5
unknown <geekisaprogrammer@gmail.com>
2023-06-19 19:50:05 +0530

    first commit

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // track the connection
+
+export const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "Promptopia",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("MongodDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
